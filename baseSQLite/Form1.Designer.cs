a33e1fa39d4ce892d7f6a936c7793caee3c5da41
Alumno <Alumno@PC-39>
2022-10-28 12:26:29 -0500

    parte2

--- baseSQLite/Form1.Designer.cs
+++ baseSQLite/Form1.Designer.cs
@@ -74,6 +74,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(247, 348);
             this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // nombre
             // 
--- baseSQLite/Form1.cs
+++ baseSQLite/Form1.cs
@@ -7,11 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;//Agregamos esta libreria
+using System.Data.SqlClient;
 
 namespace baseSQLite
 {
     public partial class Form1 : Form
     {
+        //Agregamos la base de datos
+        string path = "data_table.db";
+        string cs = @"URL=file:" + Application.StartupPath + "\\data_table.db";
+        //La base de datos se queda en la carpeta DEBUG
+
+        SQLiteConnection conn;
+        SQLiteCommand cmd;
+        SQLiteDataReader dr;
         public Form1()
         {
             InitializeComponent();
@@ -24,12 +34,52 @@ namespace baseSQLite
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Create_db();
+            data_show();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void Create_db()
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                SQLiteConnection.CreateFile(path);
+                using (var sqlite = new SQLiteConnection(@"Data Source=" + path))
+                {
+                    sqlite.Open();
+                    string sql = "Create table test (name varchar(20), id varchar(12))";
+                    SQLiteCommand command = new SQLiteCommand(sql, sqlite);
+                    command.ExecuteNonQuery();
+                }
+            }
+            else
+            {
+                Console.WriteLine("La base de datos no se pudo crear");
+                return;
+            }
+        }
+        private void data_show()
+        {
+            var conn = new SQLiteConnection(cs);
+            conn.Open();
+
+            string stm = "SELECT * FROM test";
+            var cmd= new SQLiteCommand(stm, conn);
+            dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                dataGridView1.Rows.Add(0, dr.GetString(0),dr.GetString(1));  
+            }
+
+        }
+
     }
 }
